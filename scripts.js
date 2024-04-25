e9a519a7edb6fe236ba02f269f16396acdc3a13e
Marcus Rådell <marcus@radell.net>
2024-04-25 21:31:34 +0200

    style wishlist items

--- scripts.js
+++ scripts.js
@@ -11,11 +11,26 @@ const main = () => {
     } else if (hash === "#wishlist") {
       content.innerHTML = `
       <h1 class="page-title">Wishlist</h1>
-      <ul>
-        <li>Armbandsur</li>
-        <li>Ballerinaskor eller tofflor</li>
-        <li>Sommarskor</li>
-        <li>Sandaler</li>
+      <ul class="wishlist">
+        <li class="wishlist-item">
+            <h2 class="wishlist-item-title">Wristwatch</h2>
+            <div class="wishlist-item-body">
+                <img class="wishlist-item-image" src="wristwatch.jpg" alt="Wristwatch">
+                <p class="wishlist-item-description">The wristwatch she got from before doesn't work anymore.</p>
+            </div>
+        </li>
+        <li class="wishlist-item">
+            <h2>Ballerina shoes</h2>
+            <p>Either ballerina shoes, or slippers.</p>
+        </li>
+        <li class="wishlist-item">
+            <h2>Summer shoes</h2>
+            <p>Maybe dad or mom will buy her these anyhow, so she might not need to wish for them.</p>
+        </li>
+        <li class="wishlist-item">
+            <h2>Sandals</h2>
+            <p>This is a nice-to-have.</p>
+        </li>
       </ul>
       `;
     } else if (hash === "#" || !hash) {
