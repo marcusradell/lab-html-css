141eae623600361bb69ebf7ac7e7fdcb70c1a995
Marcus Rådell <marcus@radell.net>
2024-04-25 19:19:15 +0200

    add wishlist

--- scripts.js
+++ scripts.js
@@ -4,7 +4,20 @@ const main = () => {
     const content = document.getElementById("content");
 
     if (hash === "#drawings") {
-      content.innerHTML = `<h1>Drawings</h1> <img class="drawing" src="drawing1.png">`;
+      content.innerHTML = `
+      <h1>Drawings</h1>
+      <img class="drawing" src="drawing1.png">
+      `;
+    } else if (hash === "#wishlist") {
+      content.innerHTML = `
+      <h1>Wishlist</h1>
+      <ul>
+        <li>Armbandsur</li>
+        <li>Ballerinaskor eller tofflor</li>
+        <li>Sommarskor</li>
+        <li>Sandaler</li>
+      </ul>
+      `;
     } else {
       content.innerHTML = "";
     }
