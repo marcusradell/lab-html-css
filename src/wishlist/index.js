de2f8a79680bbe51c3f37adeb3e1f224a027c190
Marcus Rådell <marcus@radell.net>
2024-04-26 19:24:35 +0200

    Dan wishlist

--- src/wishlist/index.js
+++ src/wishlist/index.js
@@ -3,22 +3,44 @@ const wishes = [
     title: "Wristwatch",
     image: "wristwatch.jpg",
     description: "The wristwatch she got from before doesn't work anymore.",
+    kid: "Sol",
   },
   {
     title: "Ballerina shoes",
     image: "ballerina-shoes.jpg",
     description: "Either ballerina shoes, or slippers.",
+    kid: "Sol",
   },
   {
     title: "Summer shoes",
     image: "summer-shoes.jpg",
     description:
       "Maybe dad or mom will buy her these anyhow, so she might not need to wish for them.",
+    kid: "Sol",
   },
   {
     title: "Sandals",
     image: "sandals.webp",
     description: "This is a nice-to-have.",
+    kid: "Sol",
+  },
+  {
+    title: "Dinosaur",
+    image: "dinosaur.jpg",
+    description: "Dan wants it.",
+    kid: "Dan",
+  },
+  {
+    title: "Supervillain action figure",
+    image: "supervillain.jpg",
+    description: "Dan wants one of them.",
+    kid: "Dan",
+  },
+  {
+    title: "Spider-man series action figure",
+    image: "spider-man-series-hero.jpg",
+    description: "Dan wants a hero to fight the villain.",
+    kid: "Dan",
   },
 ];
 
