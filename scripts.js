fabf2445f2b61d2dba204dc173b32d5f887a78a2
Marcus Rådell <marcus@radell.net>
2024-04-26 08:05:04 +0200

    break out wishlist to its own file

--- scripts.js
+++ scripts.js
@@ -2,47 +2,6 @@ const welcomePage = () => `
     <h1 class="page-title">Home</h1>
     <p>Welcome to my page!</p>`;
 
-const wishes = [
-  {
-    title: "Wristwatch",
-    image: "wristwatch.jpg",
-    description: "The wristwatch she got from before doesn't work anymore.",
-  },
-  {
-    title: "Ballerina shoes",
-    image: "ballerina-shoes.jpg",
-    description: "Either ballerina shoes, or slippers.",
-  },
-  {
-    title: "Summer shoes",
-    image: "summer-shoes.jpg",
-    description:
-      "Maybe dad or mom will buy her these anyhow, so she might not need to wish for them.",
-  },
-  {
-    title: "Sandals",
-    image: "sandals.webp",
-    description: "This is a nice-to-have.",
-  },
-];
-
-const wishlistItem = (title, image, description) => `
-  <li class="wishlist-item">
-    <h2 class="wishlist-item-title">${title}</h2>
-    <div class="wishlist-item-body">
-      <img class="wishlist-item-image" src="${image}" alt="${title}">
-      <p class="wishlist-item-description">${description}</p>
-    </div>
-</li>`;
-
-const wishListPage = (wishes) => `
-    <h1 class="page-title">Wishlist</h1>
-    <ul class="wishlist">
-    ${wishes
-      .map((wish) => wishlistItem(wish.title, wish.image, wish.description))
-      .join("")}
-    </ul>`;
-
 const drawings = [{ src: "drawing1.png" }];
 
 const drawingsPage = () => `
@@ -68,7 +27,7 @@ const getRoute = (hash) => {
     case "#":
       return welcomePage();
     case "#wishlist":
-      return wishListPage(wishes);
+      return window.wishListPage();
     case "#drawings":
       return drawingsPage();
     default:
---
+++ src/wishlist/index.js
@@ -0,0 +1,40 @@
+const wishes = [
+  {
+    title: "Wristwatch",
+    image: "wristwatch.jpg",
+    description: "The wristwatch she got from before doesn't work anymore.",
+  },
+  {
+    title: "Ballerina shoes",
+    image: "ballerina-shoes.jpg",
+    description: "Either ballerina shoes, or slippers.",
+  },
+  {
+    title: "Summer shoes",
+    image: "summer-shoes.jpg",
+    description:
+      "Maybe dad or mom will buy her these anyhow, so she might not need to wish for them.",
+  },
+  {
+    title: "Sandals",
+    image: "sandals.webp",
+    description: "This is a nice-to-have.",
+  },
+];
+
+const wishlistItem = (title, image, description) => `
+  <li class="wishlist-item">
+    <h2 class="wishlist-item-title">${title}</h2>
+    <div class="wishlist-item-body">
+      <img class="wishlist-item-image" src="${image}" alt="${title}">
+      <p class="wishlist-item-description">${description}</p>
+    </div>
+</li>`;
+
+window.wishListPage = () => `
+    <h1 class="page-title">Wishlist</h1>
+    <ul class="wishlist">
+    ${wishes
+      .map((wish) => wishlistItem(wish.title, wish.image, wish.description))
+      .join("")}
+    </ul>`;
