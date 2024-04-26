3239201b414ae65cdb1e1716880bab8b88f094fc
Marcus Rådell <marcus@radell.net>
2024-04-26 08:12:51 +0200

    move drawing image

--- src/drawings/index.js
+++ src/drawings/index.js
@@ -4,6 +4,7 @@ window.drawingsPage = () => `
     <h1 class="page-title">Drawings</h1>
     ${drawings
       .map(
-        (drawing) => `<img class="drawing" src="${drawing.src}" alt="Drawing">`
+        (drawing) =>
+          `<img class="drawing" src="src/drawings/${drawing.src}" alt="Drawing">`
       )
       .join("")}`;
